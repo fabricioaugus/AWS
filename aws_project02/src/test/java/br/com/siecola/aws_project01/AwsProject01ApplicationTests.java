a8162c4fbc2f8081d9764ecfb2577ec015eb0ff3
fabricioaugus <49557503+fabricioaugus@users.noreply.github.com>
2022-04-22 09:32:11 -0300

    troca de gereciador de pacotes
    troca de Gerenciador de pacotes mvn para Gradle so para agilizar o curso

--- aws_project02/src/test/java/br/com/siecola/aws_project01/AwsProject01ApplicationTests.java
+++ aws_project02/src/test/java/br/com/siecola/aws_project01/AwsProject01ApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.siecola.aws_project02;
+package br.com.siecola.aws_project01;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AwsProject02ApplicationTests {
+class AwsProject01ApplicationTests {
 
 	@Test
 	void contextLoads() {
